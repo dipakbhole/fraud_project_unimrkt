2cd0dccd342a453dc36ef73155e317a2a0785fa6
Dipak Bhole <dipakbhole62@gmail.com>
2025-05-26 13:06:33 +0530

    changes in main.py

--- main.py
+++ main.py
@@ -63,5 +63,5 @@ def check_email(input_data: EmailInput):
         "verdict": "REJECTED" if matches else "ACCEPTED"
     }
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
